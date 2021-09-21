25ddd6e3888ec50b40e742adca5c0a7c0122a53c
Sergio Eduardo Flores Jr <sergio.flores.junior17@gmail.com>
2021-09-21 11:13:41 -0300

    feat: add auth

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <button @click="this.connect">Connect</button>
     <button @click="this.send">Send</button>
+    <button @click="this.auth">Authenticate</button>
     <button @click="this.disconnect">Disconnect</button>
     <pre>{{ messages.join('\n') }}</pre>
   </div>
@@ -15,6 +16,10 @@ export default {
       messages: [],
       websocket: undefined,
       eventType: 'test',
+      accountId: '1d0252a0-fbb9-4520-a6b0-b24d8206318d',
+      token: 'eyJraWQiOiJXXC9JTFRkaEg0cjNibk5BSW5LRVowaGo0aWFmSUVzelJvXC9DKzA5bGVDbkU9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI0ZWMzMjE0ZS01YjM0LTQ1OGItOWIxYi0yOTI0NTJkYjZjNGUiLCJjb2duaXRvOmdyb3VwcyI6WyJJTlZFU1RPUiJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9qQkNXanJ2cDAiLCJjb2duaXRvOnVzZXJuYW1lIjoiNGVjMzIxNGUtNWIzNC00NThiLTliMWItMjkyNDUyZGI2YzRlIiwidHlwZSI6IklOVkVTVE9SIiwiZ2l2ZW5fbmFtZSI6IlNlcmdpbyIsImFjY291bnRJZCI6IjFkMDI1MmEwLWZiYjktNDUyMC1hNmIwLWIyNGQ4MjA2MzE4ZCIsImF1ZCI6IjFhbjNrYTIzZGs5MHBqOThhOGVtOGc0bHZoIiwiR0JfQURNSU4iOiJmYWxzZSIsInRva2VuX3VzZSI6ImlkIiwicHJvZmlsZUlkIjoiNzEyOGVjNTItYWY0YS00ZmQzLWI5ZDktZmYwZWVmYzk1ZWM2IiwiSU5WRVNUT1IiOiJ7XCIwMjdjMDc0Ny03M2I5LTQwYWYtYTUzYy03NTA0NjdkYWUyYjBcIjp7XCJ0eXBlXCI6XCJJTlZFU1RPUlwiLFwicHJvZmlsZUlkXCI6XCI5ODRkMDRhYS0yMTViLTQ2ZDMtYWVmMy03Zjg5MDI5MDRmZGFcIn0sXCIxZDAyNTJhMC1mYmI5LTQ1MjAtYTZiMC1iMjRkODIwNjMxOGRcIjp7XCJ0eXBlXCI6XCJJTlZFU1RPUlwiLFwicHJvZmlsZUlkXCI6XCI3MTI4ZWM1Mi1hZjRhLTRmZDMtYjlkOS1mZjBlZWZjOTVlYzZcIn0sXCI3MDBiNWJiNi03NTQxLTQxMGQtYTFjMS03OGRlZDk2YjJiZTNcIjp7XCJ0eXBlXCI6XCJJTlZFU1RPUlwiLFwicHJvZmlsZUlkXCI6XCI5NDZlOWQxZS0wZmJkLTRjNDMtYjMyYi0yN2VmNjM5Nzc5NmNcIn19IiwiYXV0aF90aW1lIjoxNjI2NDU3NTE5LCJwaG9uZV9udW1iZXIiOiIrNTU0Nzk5MTY0ODk0NiIsImV4cCI6MTYyNjcxOTU0MCwiaWF0IjoxNjI2NzE1OTQwLCJmYW1pbHlfbmFtZSI6IkNvbnRhY3QiLCJlbWFpbCI6InNlcmdpbytjb250YWN0QGdyb3VuZGJyZWFrZXIuY28ifQ.ThjFAv-VyEVkZ7Mq2YG9xoBorPnSZ3AwMCADZNXttbD0p8h2wNz1M0LQDsmjLstRB6RedwMoT4B7ym_3kPeq_C407J93aRrEPAHFYRpHqXaz_rhKXm9bCtl6UOOiQfS0nHz7Jb4lmuWTwffGqp0txkZ9f5HUn5qrFPx-h2YTBDEdCb54nokdteFovZP2pkcWosAK4tiO3-4cBOlNZG4GsuOHeOWsrXc3MTdzWDt1hvciWh6NNAUp47EO_mK73je5DVtyG0eHtTL26wVQBlxJzraDNhPYJd1hC0I9qGIA2CYH6S-U51ZrGwKrEJOOoRJ_rpvoW9ByjHr-sDbs5-aRaA',
+      userRole: 'INVESTOR',
+      userId: '6897a769-587a-4b1b-a36f-d5fa66105bcb',
     };
   },
   methods: {
@@ -26,7 +31,7 @@ export default {
 
       // WebSocket sends a message to API Gateway on creation that gets
       // routed to the '$connect' route
-      this.websocket = new WebSocket(`${this.$WS_HOST}/?type=${this.eventType}`);
+      this.websocket = new WebSocket(`${this.$WS_HOST}/?type=${this.eventType}&userRole=${this.userRole}&userId=${this.userId}&accountId=${this.accountId}`);
 
       console.log('this.websocket', this.websocket);
 
@@ -58,6 +63,14 @@ export default {
       this.websocket.send(
         // This message will be routed to 'routeA' based on the 'action' property
         JSON.stringify({ action: 'routeA', data: 'Hello from client.' }),
+      );
+    },
+    auth() {
+      this.messages.push('client: authenticating.');
+
+      this.websocket.send(
+        // This message will be routed to 'routeA' based on the 'action' property
+        JSON.stringify({ action: 'authorize', data: JSON.stringify({ token: this.token, accountId: this.accountId }) }),
       );
     },
     disconnect() {
